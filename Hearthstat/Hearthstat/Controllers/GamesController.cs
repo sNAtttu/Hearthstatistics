777498d0f4ffb05cc51e618bb2e0f1f6e85c8d9e
sNAtttu <nsanteri@hotmail.com>
2015-07-14 22:12:57 +0300

    GameController exercise

--- Hearthstat/Hearthstat/Controllers/GamesController.cs
+++ Hearthstat/Hearthstat/Controllers/GamesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hearthstat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,10 +11,28 @@ namespace Hearthstat.Controllers
     [RoutePrefix("api/Games")]
     public class GamesController : ApiController
     {
-
+        // Get api/Games
         public string Get()
         {
             return "Antti";
+        }
+        // POST api/Games
+        public string Post([FromBody]dynamic newCard)
+        {
+            try
+            {
+                CardModel card = new CardModel
+                {
+                    AP = newCard.AP.Value,
+                    HP = newCard.HP.Value,
+                    name = newCard.name.Value
+                };
+                return "New card with name " + card.name + " has been added";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
     }
